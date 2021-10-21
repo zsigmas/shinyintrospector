b2048b3bc9b9e1b40b0e2b875a5d2d7f7da15bbf
lmoris <luis.moris.fernandez@gmail.com>
2021-10-21 13:26:26 +0200

    reorder UI

--- R/instropector.R
+++ R/instropector.R
@@ -1,28 +1,58 @@
 
 introspector_UI <- function(id) {
   ns <- shiny::NS(id)
-    shiny::fixedPanel(
-      shiny::div(
-        shiny::h2("Introspector"),
-        shiny::actionButton(
-          ns("browse_button"),
-          label = "browse"
-        ),
-        shiny::actionButton(
-          ns("update_session"),
-          label = "update_session object"
-        ),
-        shiny::tagAppendAttributes(shiny::verbatimTextOutput(ns("session_object")),
-                                        style = "width: 100%;height: 200px;overflow: scroll;"),
-        shiny::tagAppendAttributes(shiny::verbatimTextOutput(ns("input_object")),
-                                   style = "width: 100%;height: 200px;overflow: scroll;"),
-        class = "border border-primary",
-        style = "width: 100%;height: 400px;overflow: scroll;"
-      ),
-      bottom = 10,
-      left = 10,
-      draggable = T
+
+  header <- shiny::tagList(
+    shiny::h2("Introspector")
+  )
+
+  footer <- shiny::tagList(
+    shiny::actionButton(
+      ns("browse_button"),
+      label = "browse"
+    ),
+    shiny::actionButton(
+      ns("update_session"),
+      label = "update_session object"
     )
+  )
+
+  session_output <- shiny::tagAppendAttributes(
+    shiny::verbatimTextOutput(ns("session_object")),
+    style = "width: 100%;height: 200px;overflow: scroll;")
+
+  input_output <- shiny::tagAppendAttributes(
+    shiny::verbatimTextOutput(ns("input_object")),
+    style = "width: 100%;height: 200px;overflow: scroll;")
+
+  shiny::tagList(
+    shiny::fixedPanel(
+      header,
+        shiny::tabsetPanel(
+          shiny::tabPanel(
+            title = "Input",
+            input_output
+          ),
+          shiny::tabPanel(
+            title = "Session",
+            session_output
+          ),
+          id = ns("tabPanel"),
+          type = "pills"
+        ),
+      footer,
+        bottom = 10,
+        left = 10,
+        draggable = T
+    ),
+    shiny::tags$script(
+      paste0(
+        "document.getElementById('",
+        ns("browse_button"),
+        "').parentElement.parentElement.style.display = 'block';"
+      )
+    )
+  )
 }
 
 introspector_server <- function(id) {
@@ -39,7 +69,8 @@ introspector_server <- function(id) {
       input$update_session
       root_session <- shiny:::find_ancestor_session(session)
       input_list <- shiny::reactiveValuesToList(root_session$input)
-      glue::collapse(glue::glue("{names(input_list)}: {input_list}"), sep = "\n")
+      input_list <- input_list[sort(names(input_list))]
+      paste(names(input_list), ":", input_list, collapse = "\n")
     })
 
     output$session_object <- shiny::renderText({
